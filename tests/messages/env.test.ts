a2967e706efbdf3c56a599ccdd679fcf93d32f2e
Lucas <lucasgsm88@gmail.com>
2019-09-29 00:29:48 -0300

    tests with mocha

--- tests/messages/env.test.ts
+++ tests/messages/env.test.ts
@@ -1,9 +1,32 @@
-import it, { command, cases } from "../../lib/src/corde";
+import { describe } from "mocha";
+import { command, afterLogin } from "../../lib/src/corde";
+import { expect } from "chai";
+import { default as cordIt } from "../../lib/src/corde";
 
-cases(async () => {
-    await it("should return Hello", async () => {
-         await command("hello").shouldRespond("hello!!");
-         await command("hey").shouldRespond("hey!!");
+async function testShouldResponse(commandName: string, response: string) {
+    return new Promise(async (resolve) => {
+        afterLogin(async () => {
+            await cordIt("should return Hello", async () => {
+                const result = await command(commandName).shouldRespond(response);
+                resolve(result);
+            });
+        }, true);
+    });
+}
+
+describe("command hello return true", () => {
+    it("Should return hello", function (done) {
+        testShouldResponse("hello", "hello!!").then(response => {
+            expect(response).equal(true);
+            done();
+        });
     });
 });
 
+describe("command hello return true", async () => {
+    it("Should return hello", () => {
+        testShouldResponse("hello", "hello!!").then(response => {
+            expect(response).equal(true);
+        });
+    });
+});
