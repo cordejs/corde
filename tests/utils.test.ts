943b688af0401e19c2b7c0be6cff20ad0cdade5e
Lucas Gomes <lucasgsm88@gmail.com>
2020-04-21 13:41:10 -0300

    chore: add tests for utils

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,7 +1,7 @@
 import { Utils } from '../src/utils/utils';
 import { ParameterNotFoundError } from '../src/errors';
 
-describe('utils', () => {
+describe('utils Clone', () => {
   it('Object should not be equal to its clone', () => {
     expect(Utils.clone({ obj: 1 })).not.toBe({ obj: 1 });
   });
@@ -20,5 +20,16 @@ describe('utils', () => {
     } catch (error) {
       expect(error instanceof ParameterNotFoundError).toEqual(true);
     }
+  });
+});
+
+describe('utils stringfyIfObject', () => {
+  it('string should return string normal', () => {
+    expect(Utils.stringfyIfObject('teste')).toEqual('teste');
+  });
+
+  it('Object should return stringfied', () => {
+    const obj = { test: 12 };
+    expect(Utils.stringfyIfObject(obj)).toEqual(JSON.stringify(obj));
   });
 });
