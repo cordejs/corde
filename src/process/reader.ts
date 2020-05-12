d8816e2dfbf0efcbb9940b625c00e251ed8d0b8d
Lucas Gomes <lucasgsm88@gmail.com>
2020-05-11 23:45:20 -0300

    fix: build

--- src/process/reader.ts
+++ src/process/reader.ts
@@ -1,31 +1,17 @@
 import { FilesNotFoundError } from '../errors';
-import path from 'path';
 import Thread from '../building/thread';
-import fs from 'fs';
-import * as ts from 'typescript';
+import path from 'path';
 
 export async function getTestList(files: string[]) {
   if (files) {
-    const cwd = process.cwd();
     Thread.isBuildRunning = true;
     Thread.groups = [];
     for (const i in files) {
       if (files.hasOwnProperty(i)) {
-        console.log('\nFILE PATH: ' + files[i]);
-        const _path = require.resolve(files[i]);
-        const dir = 'D:/github/corde/cordeTest/test';
-        console.log('RESOLVE DIR: ' + path);
-        console.log('DIR LOCAL: ' + path.relative(__dirname, files[i]));
-        process.execPath = dir;
-        process.chdir(dir);
-        __dirname = dir;
-        const file = fs.readFileSync('../../cordeTest/test/bot.test.ts').toString();
-        eval(file);
+        require(files[i]);
       }
     }
-    process.chdir(cwd);
     Thread.isBuildRunning = false;
-    console.log(Thread.groups);
     return Thread.groups;
   }
   throw new FilesNotFoundError();
