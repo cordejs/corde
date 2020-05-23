f87b407f74130f33e2540601465d5c49207889ae
Lucas Gomes <lucasgsm88@gmail.com>
2020-05-23 19:55:43 -0300

    chore: adjusts in util functions
    - add function to pick values from a object
    - make clone function Generic

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,14 @@
 import { ParameterNotFoundError } from '../errors';
 
-export function clone(object: object | object[]): object | object[] {
+export function clone<T>(object: T | T[]): T | T[] {
   if (!object) {
     throw new ParameterNotFoundError('Object can not be undefined');
   }
   return JSON.parse(JSON.stringify(object));
+}
+
+export function pick<T, K extends keyof T>(obj: T, ...keys: K[]): Pick<T, K> {
+  const copy = {} as Pick<T, K>;
+  keys.forEach((key) => (copy[key] = obj[key]));
+  return copy;
 }
