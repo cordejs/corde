17efae4d1ddb6d95c41b6e7c1257248117f41605
Lucas Gomes <lucasgsm88@gmail.com>
2020-05-24 20:25:00 -0300

    chore: discommented code

--- corde_test/test/bot.test.js
+++ corde_test/test/bot.test.js
@@ -5,23 +5,20 @@ beforeStart(() => {
   loginBot();
 });
 
-// Testing group function
-// group('main commands', () => {
-//   test('Hello command should return... hello!!', () => {
-//     command('hello').shouldReturn('hello!!');
-//   });
+group('main commands', () => {
+  test('Hello command should return... hello!!', () => {
+    command('hello').shouldReturn('hello!!');
+  });
 
-//   test('Embed command should return a embed message!!', () => {
-//     command('embed').shouldReturn(embedMsg);
-//   });
-// });
+  test('Embed command should return a embed message!!', () => {
+    command('embed').shouldReturn(embedMsg);
+  });
+});
 
-// Testing test function
 test('Hello command should return... hello!!', () => {
   command('hello').shouldReturn('hello!!');
 });
 
-// testing Alone command
 command('hello').shouldReturn('hello!!');
 
 afterAll(() => {
