dd253c98645475d955ebeedde957aca797a34259
Lucas Gomes <lucasgsm88@gmail.com>
2020-06-13 19:15:49 -0300

    chore: remove function from main

--- src/cli.ts
+++ src/cli.ts
@@ -4,29 +4,27 @@ import init from './commands/init';
 import { configFileType } from './models';
 import { runTestsFromConfigs } from './process/engine';
 
-export function main(args: string[]) {
-  const program = new Command();
+const program = new Command();
 
-  // Add basic information with default run all command
-  program
-    .name('Corde')
-    .usage('to start testings o corde [option] to use a specific command.')
-    .description(pack.description)
-    .version(pack.version, '-v')
-    .action(async () => {
-      await runTestsFromConfigs();
-    });
+// Add basic information with default run all command
+program
+  .name('Corde')
+  .usage('to start testings o corde [option] to use a specific command.')
+  .description(pack.description)
+  .version(pack.version, '-v')
+  .action(async () => {
+    await runTestsFromConfigs();
+  });
 
-  // Add init command
-  program
-    .command('init [type]')
-    .alias('i')
-    .description('Initialize a config file with all possible options')
-    .usage('[js ts json] or empty for default type (json)')
-    .action((type: configFileType) => {
-      init(type);
-      process.exit(0);
-    });
+// Add init command
+program
+  .command('init [type]')
+  .alias('i')
+  .description('Initialize a config file with all possible options')
+  .usage('[js ts json] or empty for default type (json)')
+  .action((type: configFileType) => {
+    init(type);
+    process.exit(0);
+  });
 
-  program.parse(args);
-}
+program.parse(process.argv);
