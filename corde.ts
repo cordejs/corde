8fd6b32a6ac2e34a9bb60bdf56a32fd6ac823d94
Lucas Gomes <lucasgsm88@gmail.com>
2020-06-20 18:43:24 -0300

    fix: add data tests info

---
+++ corde.ts
@@ -0,0 +1,10 @@
+export = {
+  botPrefix: process.env.BOT_PREFIX,
+  botTestId: process.env.BOT_TEST_ID,
+  channelId: process.env.CHANNEL_ID,
+  cordeTestToken: process.env.CORDE_TEST_TOKEN,
+  guildId: process.env.GUILD_ID,
+  testFilesDir: process.env.TEST_FILES_DIR,
+  botTestToken: process.env.BOT_TEST_TOKEN,
+  timeOut: process.env.TIME_OUT,
+};
