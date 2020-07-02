4a1d40f559cd6dfd65998d173db0e1b589c11db8
Lucas Gomes <lucasgsm88@gmail.com>
2020-07-01 21:28:10 -0300

    chore: remove not used function yet

--- src/testing-api/commandOptions/mustRemoveReaction.ts
+++
@@ -1,8 +0,0 @@
-import { CordeBot } from '../../common';
-
-export async function mustRemoveReactionFnImpl(
-  isNot: boolean,
-  commandName: string,
-  cordeBot: CordeBot,
-  reactions: string[],
-) {}
