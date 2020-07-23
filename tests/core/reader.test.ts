5145ec77407a7f797d21c249add6c4b280804500
Lucas Gomes <lucasgsm88@gmail.com>
2020-07-23 20:13:55 -0300

    fix(tests): reader.tests

--- tests/core/reader.test.ts
+++ tests/core/reader.test.ts
@@ -1,76 +1,69 @@
 import reader from "../../src/core/reader";
-import {
-  renameConfigFilesToTempNames,
-  renameConfigTempFileNamesToNormal,
-  normalJsPath,
-} from "../testHelper";
 import { runtime } from "../../src/common";
 
-const conf = require("../../corde");
 import path from "path";
-import { FsMockUtils } from "../mockUtils/fs";
-
 import fs from "fs";
-jest.mock("fs");
+
+const conf = require("../mocks/jsconfig/corde.js");
+
+const cwd = process.cwd();
 
 afterEach(() => {
   runtime.configFilePath = null;
+  process.chdir(cwd);
+  jest.clearAllMocks();
 });
 
-describe("Testing reader", () => {
-  it("Instance must exist", () => {
-    expect(reader).toBeTruthy();
+describe("reader class", () => {
+  describe("when instantializing it", () => {
+    it("should create the instance", () => {
+      expect(reader).toBeTruthy();
+    });
   });
 
-  it("Should read configs from configFilePath", () => {
-    fs.readFileSync = jest.fn();
-    (fs.existsSync as jest.Mock).mockReturnValue(true);
-    runtime.configFilePath = path.resolve(process.cwd(), "tests/mocks/corde.js");
-    expect(reader.loadConfig()).toEqual(conf);
+  describe("when working with reader.loadConfig()", () => {
+    describe("and has runtime.configFilePath", () => {
+      it("should read configs from configFilePath", () => {
+        jest.spyOn(fs, "readFileSync").mockReturnValueOnce(null);
+        runtime.configFilePath = path.resolve(process.cwd(), "tests/mocks/jsconfig/corde.js");
+        expect(reader.loadConfig()).toEqual(conf);
+      });
+
+      it("should throw error when path in runtime.configFilepath is invalid", () => {
+        runtime.configFilePath = ".";
+        expect(() => reader.loadConfig()).toThrowError();
+      });
+    });
+
+    describe("when working with auto search for config files (js, ts, json)", () => {
+      it("should read configs from corde.js", () => {
+        process.chdir(path.resolve(process.cwd(), "tests/mocks/jsconfig"));
+        expect(reader.loadConfig()).toEqual(conf);
+      });
+
+      it("should read configs from corde.ts", () => {
+        process.chdir(path.resolve(process.cwd(), "tests/mocks/tsconfig"));
+        expect(reader.loadConfig()).toEqual(conf);
+      });
+
+      it("should read configs from corde.json", () => {
+        const jsonPath = path.resolve(process.cwd(), "tests/mocks/jsonconfig/corde.json");
+        const jsonConfig = JSON.parse(fs.readFileSync(jsonPath).toString());
+        process.chdir(path.resolve(process.cwd(), "tests/mocks/jsonconfig"));
+        expect(reader.loadConfig()).toEqual(jsonConfig);
+      });
+
+      it("should throw error when a config file (corde.json) is empty", () => {
+        process.chdir(path.resolve(process.cwd(), "tests/mocks/jsconfig"));
+        jest.spyOn(fs, "existsSync").mockReturnValue(true);
+        jest.spyOn(JSON, "parse").mockReturnValue(null);
+        expect(() => reader.loadConfig()).toThrowError();
+      });
+
+      it("should throw error if has no config file", () => {
+        jest.spyOn(fs, "existsSync").mockReturnValue(false);
+        expect(() => reader.loadConfig()).toThrowError();
+      });
+    });
   });
-
-  it("Must read configs from corde.js", () => {
-    jest.unmock("fs");
-    (fs.readFileSync as jest.Mock).mockReturnValueOnce(JSON.stringify({ obj: 1 }));
-    expect(reader.loadConfig()).toEqual(conf);
-  });
-
-  // it("Must read configs from corde.ts", () => {
-  //   // The file used for general tests is corde.js so, we rename it to __code.js to test other files
-  //   renameConfigFilesToTempNames();
-  //   const file = path.resolve(process.cwd(), "corde.ts");
-  //   try {
-  //     fs.writeFileSync(file, `module.exports = ${JSON.stringify(conf)}`);
-  //   } catch (error) {}
-  //   expect(reader.loadConfig()).toEqual(conf);
-  //   fs.unlinkSync(file);
-  // });
-
-  // it("Must read configs from corde.json", () => {
-  //   // The file used for general tests is corde.js so, we rename it to __code.js to test other files
-  //   renameConfigFilesToTempNames();
-  //   const file = path.resolve(process.cwd(), "corde.json");
-  //   fs.writeFileSync(file, JSON.stringify(conf));
-  //   expect(reader.loadConfig()).toEqual(conf);
-  //   fs.unlinkSync(file);
-  // });
-
-  // it("Should throw error due to path not found", () => {
-  //   renameConfigFilesToTempNames();
-  //   runtime.configFilePath = ".";
-  //   expect(() => reader.loadConfig()).toThrowError();
-  // });
-
-  // it("Should throw error due to empty object config", () => {
-  //   renameConfigFilesToTempNames();
-  //   const file = path.resolve(process.cwd(), "corde.json");
-  //   fs.writeFileSync(file, "");
-  //   expect(() => reader.loadConfig()).toThrowError();
-  //   fs.unlinkSync(file);
-  // });
-
-  // it("Should throw error due to no file", () => {
-  //   fsMock.mockReadFileSync();
-  //   expect(() => reader.loadConfig()).toThrowError();
-  // });
 });
