2aa620f0480461407502f43a57ad5ae03dc41924
Lucas Gomes <lucasgsm88@gmail.com>
2020-10-24 16:10:46 -0300

    feat: add Guid generator encapulation

---
+++ src/utils/guid.ts
@@ -0,0 +1,17 @@
+import * as uuid from "uuid";
+
+class GuidManager {
+  public new() {
+    return uuid.v4();
+  }
+
+  public validate(guid: string) {
+    return uuid.validate(guid);
+  }
+
+  public parse(guid: string) {
+    return uuid.parse(guid);
+  }
+}
+
+export const Guid = new GuidManager();
