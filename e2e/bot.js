c2db420a69796fe8b01556b2a03cdfabaa54378a
Lucas Gomes <lucasgsm88@gmail.com>
2021-01-18 22:21:25 -0300

    fix: convert bot and corde.config of e2e from .ts to .js because node do not recognize node imports '-'

--- e2e/bot.js
+++ e2e/bot.js
@@ -3,11 +3,39 @@
 // import the submodule. To avoid the problem, this file is here is root of the project,
 // but it should be in ./e2e
 // Bug to fix: https://github.com/lucasgmagalhaes/corde/issues/490
+//
+// For some reason unknown by god, ts files get error, but js files works ok (???)
+// (it's why this is the only file that is js file in this folder)
 
 import { Client, Message } from "discord.js";
-import * as config from "./e2e/corde.config";
+import * as config from "./corde.config";
 
 export const bot = new Client();
+
+/**
+ * @param {string} message
+ */
+export async function sendMessage(message) {
+  const channel = bot.channels.cache.get(config.channelId);
+
+  console.log(bot.channels);
+  if (channel === undefined) {
+    throw new Error("Channelds not loaded");
+  }
+
+  if (channel.isText()) {
+    return await channel.send(message);
+  }
+  return null;
+}
+
+/**
+ * Use this functions before use sendMessage (add it to **corde.beforeStart**)
+ */
+export async function login() {
+  console.log(config.botTestToken);
+  await bot.login(config.botTestToken);
+}
 
 bot.on("message", async (message) => {
   const args = message.content.slice(config.botPrefix.length).trim().split(" ");
@@ -31,35 +59,64 @@ bot.on("message", async (message) => {
   }
 });
 
-async function emoji(msg: Message) {
+/**
+ * @param {Message} msg
+ */
+async function emoji(msg) {
   await msg.react("😄");
 }
 
-async function edit(msg: Message) {
+/**
+ * @param {Message} msg
+ */
+async function edit(msg) {
   await msg.edit("newValue");
 }
 
-async function pin(msg: Message, msgId: string) {
+/**
+ * @param {Message} msg
+ * @param {string} msgId
+ */
+async function pin(msg, msgId) {
   const messageToPin = await msg.channel.messages.fetch(msgId);
   await messageToPin.pin();
 }
 
-async function unPin(msg: Message, msgId: string) {
+/**
+ * @param {Message} msg
+ * @param {string} msgId
+ */
+async function unPin(msg, msgId) {
   const messageToPin = await msg.channel.messages.fetch(msgId);
   await messageToPin.unpin();
 }
 
-async function addReaction(msg: Message, msgId: string, reaction: string) {
+/**
+ * @param {Message} msg
+ * @param {string} msgId
+ * @param {string} reaction
+ */
+async function addReaction(msg, msgId, reaction) {
   const message = await msg.channel.messages.fetch(msgId);
   await message.react(reaction);
 }
 
-async function editMessage(msg: Message, msgId: string, msgNewValue: string) {
+/**
+ * @param {Message} msg
+ * @param {string} msgId
+ * @param {string} msgNewValue
+ */
+async function editMessage(msg, msgId, msgNewValue) {
   const message = await msg.channel.messages.fetch(msgId);
   await message.edit(msgNewValue);
 }
 
-async function removeReaction(msg: Message, msgId: string, reaction: string) {
+/**
+ * @param {Message} msg
+ * @param {string} msgId
+ * @param {string} reaction
+ */
+async function removeReaction(msg, msgId, reaction) {
   const message = await msg.channel.messages.fetch(msgId);
   const react = message.reactions.cache.get(reaction);
 
@@ -67,12 +124,4 @@ async function removeReaction(msg: Message, msgId: string, reaction: string) {
     await react.remove();
   }
   console.log("reaction not found");
-}
-
-export async function sendMessage(message: string) {
-  const channel = bot.channels.cache.get(config.channelId);
-  if (channel.isText()) {
-    return await channel.send(message);
-  }
-  return null;
 }
--- e2e/corde.config.js
+++ e2e/corde.config.js
@@ -1,3 +1,9 @@
+// This file must be .js. If it's .ts
+// for some reason, tests will throw
+// Error: Cannot find module './corde.config'
+// A task to fix this problem is informed on
+// https://github.com/lucasgmagalhaes/corde/issues/490
+
 import env from "dotenv";
 
 var result = env.config();
