5b91dbed74b7a0dfe32be2b752abaf8be63772a3
Lucas Gomes <lucasgsm88@gmail.com>
2021-02-06 18:41:41 -0300

    feat: fix getRole and add comments to it (#516)

--- src/api/getRole.ts
+++ src/api/getRole.ts
@@ -1,10 +1,35 @@
 import { runtime } from "../common";
+import { CordeClientError } from "../errors";
 import { Role } from "../structures/role";
 import { RoleData } from "../types";
 
+/**
+ * Reach for a `role` in the informed guild in settings,
+ * basing on it's **id** or **name**.
+ *
+ * @param id Id of the role.
+ * @throws CordeClientError if corde's bot is not connected.
+ * @returns Role that matches the provided **id** or **name**
+ */
 export function getRole(id: string): Role;
+/**
+ * Reach for a `role` in the informed guild in settings,
+ * basing on it's **id** or **name**.
+ *
+ * @param data Data of the role. It can be it's **name** or **id**.
+ * if both informations be provided, and they are from two differents
+ * roles, the result will correspond to the role that matchs with the parameter
+ * **id**.
+ *
+ * @throws CordeClientError if corde's bot is not connected.
+ * @returns Role that matches the provided **id** or **name**
+ */
 export function getRole(data: RoleData): Role;
 export function getRole(data: string | RoleData) {
+  if (!runtime.isBotLoggedIn()) {
+    throw new CordeClientError("bot is not connected yet. No role can be searched");
+  }
+
   if (typeof data === "string") {
     return runtime.bot.getRoles().find((r) => r.id === data);
   }
