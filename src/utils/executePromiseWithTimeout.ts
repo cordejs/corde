66adb5c29d4681afa60cd251512a74363bf34516
Lucas Gomes <lucasgsm88@gmail.com>
2021-03-22 15:44:51 -0300

    feat: enhanced executePromiseWithTimeout

--- src/utils/executePromiseWithTimeout.ts
+++ src/utils/executePromiseWithTimeout.ts
@@ -1,3 +1,4 @@
+import { TimeoutError } from "../errors";
 import { RejectFunction, ResolveFunction } from "../types";
 
 /**
@@ -9,22 +10,34 @@ import { RejectFunction, ResolveFunction } from "../types";
  * @example
  *
  * function example(shouldPass: boolean) {
- *   return executePromiseWithTimeout((resolve, reject) => {
+ *   const items: string[] = [];
+ *   items.push("item 1");
+ *   return executePromiseWithTimeout<string[]>((resolve, reject) => {
  *      if(shouldPass) {
- *         resolve();
+ *         items.push("item 2");
+ *         resolve(items);
  *      } else {
  *         reject();
  *      }
- *   }, 1000);
+ *   }, 1000, items);
  * }
+ *
+ * // If the functions reachs the timeout, it will throw the
+ * // exception TimeoutError with the items array as data property
+ *
+ * @throws TimeoutError if the function reachs timeout. This error has a property
+ * data that is passed throw the `rejectedData` parameter
  *
  * @param fn function to be executed.
  * @param timeout Time to wait for completion of the function.
+ * @param rejectedData Data that should be added in rejection if the function reachs timeout.
  * @returns Return of `fn` parameter.
+ * @internal
  */
 export function executePromiseWithTimeout<TResult extends any>(
   fn: (resolve: ResolveFunction<TResult>, reject: RejectFunction) => void,
   timeout: number,
+  rejectedData?: TResult,
 ) {
   if (!fn) {
     throw new Error("can not execute an null function");
@@ -32,7 +45,11 @@ export function executePromiseWithTimeout<TResult extends any>(
 
   return new Promise<TResult>(async (resolve, reject) => {
     const nodeTimeout = setTimeout(() => {
-      reject("timeout");
+      if (rejectedData) {
+        reject(new TimeoutError("timeout", rejectedData));
+      } else {
+        reject(new TimeoutError("timeout"));
+      }
     }, timeout);
 
     fn((value) => {
--- tests/utils/executePromiseWithTimeout.test.ts
+++ tests/utils/executePromiseWithTimeout.test.ts
@@ -1,3 +1,4 @@
+import { TimeoutError } from "../../src/errors";
 import { executePromiseWithTimeout } from "../../src/utils";
 
 describe("testing executePromiseWithTimeout", () => {
@@ -9,19 +10,39 @@ describe("testing executePromiseWithTimeout", () => {
     expect(num).toEqual(1);
   });
 
-  it("should reject", () => {
-    expect(
-      async () => await executePromiseWithTimeout((_, reject) => reject(), 1000),
-    ).rejects.toBeTruthy();
+  it("should reject", async () => {
+    try {
+      await executePromiseWithTimeout((_, reject) => reject("self rejection"), 1000);
+      fail();
+    } catch (error) {
+      expect(error).toEqual("self rejection");
+    }
   });
 
-  it("should get timeout due to function not resolved or rejected", () => {
-    expect(
-      async () => await executePromiseWithTimeout((_, reject) => {}, 100),
-    ).rejects.toBeTruthy();
+  it("should get timeout due to function not resolved or rejected", async () => {
+    try {
+      await executePromiseWithTimeout(() => {}, 100);
+      fail();
+    } catch (error) {
+      expect(error).toBeInstanceOf(TimeoutError);
+    }
   });
 
-  it("should throw error due to no function", () => {
-    expect(async () => await executePromiseWithTimeout(null, 1000)).rejects.toBeTruthy();
+  it("should throw error due to no function", async () => {
+    try {
+      await executePromiseWithTimeout(null, 1000);
+      fail();
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+    }
+  });
+
+  it("should throw a error with a data", async () => {
+    try {
+      await executePromiseWithTimeout(() => {}, 1000, "test");
+      fail();
+    } catch (error) {
+      expect(error).toEqual(new TimeoutError("timeout", "test"));
+    }
   });
 });
