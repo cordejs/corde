c3b4babd85a2ad752666728c9c08fb8510603fb1
Lucas Gomes <lucasgsm88@gmail.com>
2021-03-24 14:07:24 -0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { corde } from "./corde";
 export { group, test } from "./clausures";
 export { expect } from "./expect";
 export { sendMessage, getRole, createRole } from "./api-utilities";
@@ -6,5 +7,4 @@ export { afterAll, beforeStart, afterEach, beforeEach } from "./hooks";
 export { RolePermission, Permission } from "./utils/permission";
 export { Colors } from "./utils/colors";
 
-import { corde } from "./corde";
 export default corde;
