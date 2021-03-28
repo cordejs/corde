d20c2e7c73f1e1c3f2981e901da380ce0b0e5eb5
Corde deployment script <lucasgsm88@gmail.com>
2021-03-28 18:35:13 -0300

    chore: update snapshots

--- e2e/toSetRolePosition/__cordeTest__/bot_case1.test.ts
+++ e2e/toSetRolePosition/__cordeTest__/bot_case1.test.ts
@@ -1,15 +1,18 @@
+// @ts-nocheck
+
 import corde from "../../../lib";
 
 let role = null;
 const roleName = "role-color";
 
-corde.test("", async () => {
+corde.test("should increase a role position", () => {
   role = corde.getRole({ name: roleName });
   corde.expect(`increaseRolePosition ${role.id}`).toSetRolePosition(role.position + 1, role.id);
 });
 
 corde.afterAll(async () => {
-  if (role) {
-    await role.updatePosition(role.position - 1);
+  const _role = role;
+  if (_role) {
+    await _role.updatePosition(_role.position - 1);
   }
 });
