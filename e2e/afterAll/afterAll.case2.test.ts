90346b11cd55f6459e000bb3ad2e77bf26448f01
Corde deployment script <lucasgsm88@gmail.com>
2021-03-28 15:22:03 -0300

    chore: remove trash tests

--- e2e/afterAll/afterAll.case2.test.ts
+++
@@ -1,9 +0,0 @@
-import { runCLI } from "../cliRunner";
-import Utils from "../testUtils";
-
-it("testing afterAll function in bot_case1 (should be fail)", async () => {
-  const command = Utils.buildCommandWithConfigPath("afterAll", "bot_case2.test.ts");
-  const [mockProcess, stdout] = await runCLI(command);
-  expect(mockProcess).toBeCalledWith(0);
-  expect(stdout).toMatchSnapshot();
-});
