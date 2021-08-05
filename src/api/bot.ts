5f66554fe276eb54eae5891b4eb6be9b28a6da8f
Lucas Magalhães <lucasgsm88@gmail.com>
2021-08-05 20:15:21 -0300

    feat: add new functions for bot

--- src/api/bot.ts
+++ src/api/bot.ts
@@ -1,7 +1,27 @@
-import { Message, Role } from "discord.js";
+import {
+  CategoryChannel,
+  DMChannel,
+  Guild,
+  Message,
+  NewsChannel,
+  Role,
+  TextChannel,
+  VoiceChannel,
+} from "discord.js";
 import { CordeClientError } from "../errors";
 import { mapper } from "../mapper/messageMapper";
-import { ICordeBot, IMessageEmbed, IRoleData, IRoleIdentifier, Primitive } from "../types";
+import {
+  IChannelIdentifier,
+  ICordeBot,
+  ICreateChannelOptions,
+  ICreateChannelOptionsSimple,
+  IGuildCreateOptions,
+  IGuildIdentifier,
+  IMessageEmbed,
+  IRoleData,
+  IRoleIdentifier,
+  Primitive,
+} from "../types";
 import { isPrimitiveValue } from "../utils";
 
 export class Bot {
@@ -14,6 +34,55 @@ export class Bot {
    */
   get voiceState() {
     return this._bot.voiceConnection;
+  }
+
+  /**
+   * Client of Discord.js
+   */
+  get client() {
+    return this._bot.client;
+  }
+
+  /**
+   * Same of `this.getChannel()`
+   */
+  get channel() {
+    return this.getChannel();
+  }
+
+  /**
+   * Get all channels in **cache** of the bot
+   */
+  get channels() {
+    return this._bot.client.channels.cache.array();
+  }
+
+  /**
+   * Same of `this.getGuild()`
+   */
+  get guild() {
+    return this.getGuild();
+  }
+
+  /**
+   * Get all guilds in **cache** of the bot
+   */
+  get guilds() {
+    return this._bot.client.guilds.cache.array();
+  }
+
+  /**
+   * Get all roles in **cache** of the guild
+   * defined in configs.
+   *
+   * Shortcut for:
+   *
+   * ```typescript
+   * this.getGuild().roles.cache.array()
+   * ```
+   */
+  get roles() {
+    return this.getGuild().roles.cache.array();
   }
 
   /**
@@ -41,8 +110,8 @@ export class Bot {
    * @param channelId Voice channel to corde's bot connect
    * @returns Voice connection state. This property can be get from `bot.voiceState`
    */
-  async joinVoiceChannel(channelId: string) {
-    return await this._bot.joinVoiceChannel(channelId);
+  joinVoiceChannel(channelId: string) {
+    return this._bot.joinVoiceChannel(channelId);
   }
 
   /**
@@ -50,6 +119,13 @@ export class Bot {
    */
   leaveVoiceChannel() {
     this._bot.leaveVoiceChannel();
+  }
+
+  /**
+   * From all channels in **cache**, get all that are of type text
+   */
+  getOnlyTextChannels() {
+    return this.channels.filter((c) => c.isText()) as (TextChannel | DMChannel | NewsChannel)[];
   }
 
   /**
@@ -64,8 +140,8 @@ export class Bot {
    * @param id Id of the channel
    * @returns Channel if it's found
    */
-  async fetchChannel(id: string) {
-    return await this._bot.fetchChannel(id);
+  fetchChannel(id: string) {
+    return this._bot.fetchChannel(id);
   }
 
   /**
@@ -78,26 +154,112 @@ export class Bot {
   }
 
   /**
+   * Fetch for a role based on it's id, caching it after that.
+   * @param roleId Id of the role.
+   * @throws Error if corde's bot isn't connected yet.
+   * @returns Fetched Role or undefined.
+   */
+  async fetchRole(roleId: string): Promise<Role | undefined>;
+  /**
+   * Fetch for a role based on it's id, and it's guild's id, caching it after that.
+   * @param roleId Id of the role.
+   * @param guildId Id of the guild.
+   * @param fetchGuild Define if the guild should be fetched or searched in cache.
+   * @throws Error if corde's bot isn't connected yet.
+   * @returns Fetched Role or undefined.
+   */
+  async fetchRole(roleId: string, guildId: string, fetchGuild?: boolean): Promise<Role | undefined>;
+  async fetchRole(roleId: string, guildId?: string, fetchGuild = false): Promise<Role | undefined> {
+    if (!this._bot.isLoggedIn()) {
+      throw new CordeClientError("Could not create the guild while corde bot isn't connected yet");
+    }
+
+    if (guildId) {
+      const guild = await this._getOrFetchRole(guildId, fetchGuild);
+
+      if (!guild) {
+        return undefined;
+      }
+
+      return this._fetchRole(roleId, this.guild);
+    }
+    return this._fetchRole(roleId, this.guild);
+  }
+
+  private _getOrFetchRole(guildId: string, fetchGuild = false) {
+    if (fetchGuild) {
+      return this._bot.client.guilds.fetch(guildId);
+    } else {
+      return this._bot.client.guilds.cache.get(guildId);
+    }
+  }
+
+  /**
    * Gets the channel defined in `configs`
    */
-  getChannel() {
-    return this._bot.channel;
+  getChannel(): TextChannel;
+  /**
+   * Gets a channel from `client.channels.cache` based on the channel's id
+   * @param id Channel Id
+   * @return Channel searched by it's id or undefined.
+   */
+  getChannel(id: string): TextChannel | undefined;
+  /**
+   * Gets a channel from `client.channels.cache` based on the channel's id or name
+   * @param identifier Channel's identifier
+   * @return Channel searched or undefined.
+   */
+  getChannel(identifier: IChannelIdentifier): TextChannel | undefined;
+  getChannel(identifier?: string | IChannelIdentifier) {
+    if (!identifier) {
+      return this._bot.channel;
+    }
+
+    if (typeof identifier === "string") {
+      return this._bot.client.channels.cache.find((c) => c.id === identifier);
+    }
+
+    return this._bot.client.channels.cache.find((c) => {
+      if (c.id === identifier.id) {
+        return true;
+      }
+
+      if (c.isText()) {
+        return (c as TextChannel).name === identifier.name;
+      }
+
+      return false;
+    });
   }
 
   /**
    * Gets the guild defined in `configs`
    */
-  getGuild() {
-    return this._bot.guild;
-  }
-
+  getGuild(): Guild;
   /**
-   * Search for a guild in guild's cache, based on it's id
-   * @param id id of the guild
-   * @returns Guild if the id is valid and the guild is in the cache
+   * Gets a guild from `client.channels.guild` based on the guild's id
+   * @param id Guild Id
+   * @return Guild searched by it's id or undefined.
    */
-  findGuild(id: string) {
-    return this._bot.findGuild(id);
+  getGuild(id: string): Guild | undefined;
+  /**
+   * Gets a guild from `client.guild.cache` based on the guild's id or name
+   * @param identifier Guild's identifier
+   * @return Guild searched or undefined.
+   */
+  getGuild(identifier: IGuildIdentifier): Guild | undefined;
+  getGuild(identifier?: string | IGuildIdentifier) {
+    if (!identifier) {
+      return this._bot.guild;
+    }
+
+    if (typeof identifier === "string") {
+      return this._bot.client.guilds.cache.find((c) => c.id === identifier);
+    }
+
+    return this._bot.client.guilds.cache.find(
+      (c) => c.id === identifier.id || c.name === identifier.name,
+    );
   }
 
   /**
@@ -111,13 +273,13 @@ export class Bot {
    *
    * // Works
    * test("test 1", () => {
-   *    const message = await sendMessage("msg");
+   *    const message = await corde.bot.send("msg");
    *    expect(`editMessage ${message.id}`).toEditMessage({ id: message.id }, "newValue");
    * });
    *
    * // Do not Works
    * group("test 1", () => {
-   *    const message = await sendMessage("msg");
+   *    const message = await corde.bot.send("msg");
    * });
    *
    * @throws CordeClienteError - If bot is not connected yet.
@@ -127,9 +289,9 @@ export class Bot {
    *
    * @since 2.0
    */
-  async send(message: string | number | boolean | bigint): Promise<Message>;
-  async send(message: IMessageEmbed): Promise<Message>;
-  async send(message: Primitive | IMessageEmbed): Promise<Message> {
+  send(message: string | number | boolean | bigint): Promise<Message>;
+  send(message: IMessageEmbed): Promise<Message>;
+  send(message: Primitive | IMessageEmbed): Promise<Message> {
     if (!message) {
       throw new Error("Can not send a empty message");
     }
@@ -141,11 +303,11 @@ export class Bot {
     }
 
     if (isPrimitiveValue(message)) {
-      return await this._bot.sendMessage(message);
+      return this._bot.sendMessage(message);
     }
 
     const embed = mapper.embedInterfaceToMessageEmbed(message);
-    return await this._bot.sendMessage(embed);
+    return this._bot.sendMessage(embed);
   }
 
   /**
@@ -171,7 +333,183 @@ export class Bot {
     }
   }
 
-  // TODO: Rename "findRole" to "getRole"
+  /**
+   * Creates a new `guild` in defined in configs.
+   *
+   * Shortcut for:
+   *
+   * ```typescript
+   * this.client.guilds.create("nameExample");
+   * ```
+   *
+   * @param name Name of the new guild.
+   * @throws Error if corde's bot isn't connected yet.
+   * @returns Created guild.
+   */
+  createGuild(name: string): Promise<Guild>;
+  /**
+   * Creates a new `guild` in defined in configs.
+   *
+   * Shortcut for:
+   *
+   * ```typescript
+   * this.client.guilds.create("exampleName", { ... });
+   * ```
+   *
+   * @param options Informations about the guild.
+   * @throws Error if corde's bot isn't connected yet.
+   * @returns Created guild.
+   */
+  createGuild(options: IGuildCreateOptions): Promise<Guild>;
+  createGuild(options: string | IGuildCreateOptions) {
+    if (!this._bot.isLoggedIn()) {
+      throw new CordeClientError("Could not create the guild while corde bot isn't connected yet");
+    }
+
+    if (typeof options === "string") {
+      return this._bot.client.guilds.create(options);
+    }
+    return this._bot.client.guilds.create(options.name, options);
+  }
+
+  /**
+   * Creates a new channel in guild defined in configs.
+   *
+   * Shortcut for:
+   *
+   * ```typescript
+   * this.client.channels.create("exampleName");
+   * ```
+   *
+   * @param name Name of the new channel.
+   * @throws Error if corde's bot isn't connected yet.
+   * @returns Created channel.
+   */
+  createChannel(name: string): Promise<TextChannel>;
+  /**
+   * Creates a new channel in guild defined in configs.
+   *
+   * Shortcut for:
+   *
+   * ```typescript
+   * this.client.channels.create("exampleName", { ... });
+   * ```
+   *
+   * @param options Informations about the channel, including it's type.
+   * @throws Error if corde's bot isn't connected yet.
+   * @returns Created channel.
+   */
+  createChannel(
+    channelOptions: ICreateChannelOptions,
+  ): Promise<TextChannel | VoiceChannel | CategoryChannel>;
+  createChannel(options: string | ICreateChannelOptions) {
+    if (!this._bot.isLoggedIn()) {
+      throw new CordeClientError(
+        "Could not create the channel while corde bot isn't connected yet",
+      );
+    }
+
+    if (typeof options === "string") {
+      return this.guild.channels.create(options);
+    }
+    return this.guild.channels.create(options.name, options);
+  }
+
+  /**
+   * Creates a new **voice** channel in guild defined in configs.
+   *
+   * Shortcut for:
+   *
+   * ```typescript
+   * this.client.channels.create("exampleName", { type: "voice" });
+   * ```
+   *
+   * @param name Name of the new channel.
+   * @throws Error if corde's bot isn't connected yet.
+   * @returns Created channel.
+   */
+  createVoiceChannel(name: string): Promise<VoiceChannel>;
+  /**
+   * Creates a new **voice** channel in guild defined in configs.
+   *
+   * Shortcut for:
+   *
+   * ```typescript
+   * this.client.channels.create("exampleName", { ..., type: "voice" });
+   * ```
+   *
+   * @param options Informations about the channel.
+   * @throws Error if corde's bot isn't connected yet.
+   * @returns Created channel.
+   */
+  createVoiceChannel(options: ICreateChannelOptionsSimple): Promise<VoiceChannel>;
+  createVoiceChannel(options: string | ICreateChannelOptionsSimple) {
+    return this._createChannel(options, "voice");
+  }
+
+  /**
+   * Creates a new **text** channel in guild defined in configs.
+   *
+   * Shortcut for:
+   *
+   * ```typescript
+   * this.client.channels.create("exampleName", { type: "text" });
+   * ```
+   *
+   * @param name Name of the new channel.
+   * @throws Error if corde's bot isn't connected yet.
+   * @returns Created channel.
+   */
+  createTextChannel(name: string): Promise<TextChannel>;
+  /**
+   * Creates a new **text** channel in guild defined in configs.
+   *
+   * Shortcut for:
+   *
+   * ```typescript
+   * this.client.channels.create("exampleName", { ..., type: "text" });
+   * ```
+   *
+   * @param options Informations about the channel.
+   * @throws Error if corde's bot isn't connected yet.
+   * @returns Created channel.
+   */
+  createTextChannel(options: ICreateChannelOptionsSimple): Promise<TextChannel>;
+  createTextChannel(options: string | ICreateChannelOptionsSimple) {
+    return this._createChannel(options, "text");
+  }
+
+  /**
+   * Creates a new **category** channel in guild defined in configs.
+   *
+   * Shortcut for:
+   *
+   * ```typescript
+   * this.client.channels.create("exampleName", { type: "category" });
+   * ```
+   *
+   * @param name Name of the new channel.
+   * @throws Error if corde's bot isn't connected yet.
+   * @returns Created channel.
+   */
+  createCategoryChannel(name: string): Promise<CategoryChannel>;
+  /**
+   * Creates a new **category** channel in guild defined in configs.
+   *
+   * Shortcut for:
+   *
+   * ```typescript
+   * this.client.channels.create("exampleName", { ..., type: "category" });
+   * ```
+   *
+   * @param options Informations about the channel.
+   * @throws Error if corde's bot isn't connected yet.
+   * @returns Created channel.
+   */
+  createCategoryChannel(options: ICreateChannelOptionsSimple): Promise<CategoryChannel>;
+  createCategoryChannel(options: string | ICreateChannelOptionsSimple) {
+    return this._createChannel(options, "category");
+  }
 
   /**
    * Finds a role in config guild's cache, basing on it's **id**
@@ -202,10 +540,34 @@ export class Bot {
     return this._getRole(data);
   }
 
+  private async _fetchRole(roleId: string, guild: Guild) {
+    const role = await guild.roles.fetch(roleId, true);
+    if (role) {
+      return role;
+    }
+    return undefined;
+  }
+
   private _getRole(data: string | IRoleIdentifier) {
     if (typeof data === "string") {
       return this._bot.getRoles().find((r) => r.id === data);
     }
     return this._bot.getRoles().find((r) => r.id === data.id || r.name === data.name);
+  }
+
+  private _createChannel(
+    options: string | ICreateChannelOptionsSimple,
+    type?: ICreateChannelOptions["type"],
+  ) {
+    if (!this._bot.isLoggedIn()) {
+      throw new CordeClientError(
+        "Could not create the channel while corde bot isn't connected yet",
+      );
+    }
+
+    if (typeof options === "string") {
+      return this.guild.channels.create(options, { type });
+    }
+    return this.guild.channels.create(options.name, options);
   }
 }
