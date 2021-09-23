2a878dcca78ab8569656be05a937d2786889d692
Lucas Gomes <lucasgsm88@gmail.com>
2021-09-23 10:07:34 -0300

    chore: fix test(it) closure tests

--- tests/closure/test.test.ts
+++ tests/closure/test.test.ts
@@ -1,11 +1,10 @@
 import { testCollector } from "../../src/common/testCollector";
-import { ITest } from "../../src/types";
 import { test as _test } from "../../src/closures";
-import { expect as cordeExpect } from "../../src/expect";
 
 describe("Testing test function", () => {
   beforeEach(() => {
-    testCollector.tests = [];
+    testCollector.clearTestFiles();
+    testCollector.createTestFile("");
   });
 
   it("Should execute test function", async () => {
@@ -14,85 +13,8 @@ describe("Testing test function", () => {
       a = 2;
     });
 
-    await testCollector.executeTestClojure();
+    expect(testCollector.currentTestFile.tests).toHaveLength(1);
+    await testCollector.currentTestFile.tests[0].action();
     expect(a).toBe(2);
   });
-
-  it("Should add a test", async () => {
-    const testName = "test group";
-    const testObj: ITest = {
-      testsFunctions: [],
-      name: testName,
-    };
-
-    _test(testName, () => {});
-
-    await testCollector.executeTestClojure();
-
-    expect(testCollector.tests.length).toEqual(0);
-  });
-
-  it("should not add a group", async () => {
-    _test(undefined, () => {});
-    await testCollector.executeTestClojure();
-
-    if (!testCollector.tests) {
-      fail();
-    } else {
-      expect(testCollector.tests.length).toEqual(0);
-    }
-  });
-
-  it("add two tests with single cases inside then should return two tests with single cases inside then", async () => {
-    _test("case 1", () => {
-      cordeExpect("").toPin({ id: "1" });
-    });
-    _test("case 2", () => {
-      cordeExpect("").toPin({ id: "1" });
-    });
-
-    await testCollector.executeTestClojure();
-    expect(testCollector.tests[0].testsFunctions.length).toEqual(1);
-    expect(testCollector.tests[1].testsFunctions.length).toEqual(1);
-  });
-
-  it("should add cases", async () => {
-    _test("case 1", () => {
-      cordeExpect("").toPin({ id: "1" });
-      cordeExpect("").toPin({ id: "1" });
-    });
-    _test("case 2", () => {
-      cordeExpect("").toPin({ id: "1" });
-      cordeExpect("").toPin({ id: "1" });
-      cordeExpect("").toPin({ id: "1" });
-    });
-
-    await testCollector.executeTestClojure();
-    expect(testCollector.tests[0].testsFunctions.length).toEqual(2);
-    expect(testCollector.tests[1].testsFunctions.length).toEqual(3);
-  });
-
-  // Fix later
-  // it("should add sub tests", () => {
-  //   _test("case 1", () => {
-  //     cordeExpect("").toPin({ id: "1" });
-  //     _test("case 2", () => {
-  //       cordeExpect("").toPin({ id: "1" });
-  //     });
-  //   });
-  //   _test("case 3", () => {
-  //     cordeExpect("").toPin({ id: "1" });
-  //     _test("case 4", () => {
-  //       cordeExpect("").toPin({ id: "1" });
-  //       _test("case 5", () => {
-  //         cordeExpect("").toPin({ id: "1" });
-  //       });
-  //     });
-  //   });
-
-  //   expect(testCollector.tests[0].testsFunctions.length).toEqual(1);
-  //   expect(testCollector.tests[0].subTests[0].testsFunctions.length).toEqual(1);
-  //   expect(testCollector.tests[1].subTests[0].testsFunctions.length).toEqual(1);
-  //   expect(testCollector.tests[1].subTests[0].subTests[0].testsFunctions.length).toEqual(1);
-  // });
 });
