0a6ae767921318248be5d992a4ba674419385290
Lucas Gomes <lucasgsm88@gmail.com>
2021-12-09 20:09:26 -0300

    chore: remove unecessary file

--- src/core/environment.ts
+++
@@ -1,7 +0,0 @@
-import { Logger, Runtime, TestCollector } from ".";
-
-const logger = new Logger(process.stdout);
-const runtime = new Runtime();
-const testCollector = TestCollector.getInstance();
-
-export { logger, runtime, testCollector };
