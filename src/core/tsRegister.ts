58c5f9de6f790d1dc79c6293865daff3a5e9bc23
Lucas Gomes <lucasgsm88@gmail.com>
2022-02-23 21:45:44 -0300

    chore: fix typo

--- src/core/tsRegister.ts
+++ src/core/tsRegister.ts
@@ -4,7 +4,7 @@ import { IConfigOptions } from "../types";
  * @internal
  * Register `ts-node` to support typescript written tests.
  * @param projectPath Path for tsconfig.json. If it is not provided,
- * is tryed to get the file from root.
+ * is tyred to get the file from root.
  */
 export function registerTsNode(configs: Pick<IConfigOptions, "project">) {
   try {
