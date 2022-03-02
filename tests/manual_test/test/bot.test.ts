7c476549ce59b89194352ac0bff82357903259dd
Lucas Gomes <lucasgsm88@gmail.com>
2022-03-02 17:15:15 -0300

    chore: fix manual testing bot imports

--- tests/manual_test/test/bot.test.ts
+++ tests/manual_test/test/bot.test.ts
@@ -1,30 +1,30 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+/// <reference types="../../lib/src/global" />
 
-import { it, expect, beforeStart, afterAll } from "../../../lib";
 import { login, bot } from "../src/bot";
 
-beforeStart(async () => {
+beforeAll(async () => {
   await login();
 });
 
 it("ping should return pong", async () => {
-  expect("ping").toReturn("pong");
+  await command("ping").should.respond("pong");
 });
 
-it("embed should return an embed message", () => {
-  expect("embed").toReturn({
+it("embed should return an embed message", async () => {
+  await command("embed").should.respond({
     description: "test",
   });
 });
 
-it("embed-partial should return an embed message", () => {
-  expect("embedPartial").toEmbedMatch({
+it("embed-partial should return an embed message", async () => {
+  await command("embedPartial").should.embedMatch({
     description: "test",
   });
 });
 
-it("embed should return an embed message", () => {
-  expect("ping").toMessageContentContains("pon");
+it("embed should return an embed message", async () => {
+  await command("ping").should.messageContentContains("pon");
 });
 
 afterAll(() => {
