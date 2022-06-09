5943540c24eff5ec9ba0f46b183176388d000c75
Lucas Gomes <lucasgsm88@gmail.com>
2022-06-09 15:26:06 -0300

    chore: remove browser sender

--- packages/browser-sender/config.js
+++
@@ -1,8 +0,0 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
-module.exports = {
-  discord_password: process.env.DISCORD_PASSWORD,
-  discord_user: process.env.DISCORD_USER,
-  server_name: process.env.SERVER_NAME,
-};
