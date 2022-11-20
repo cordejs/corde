76f469f73a63b4cc437e2e5e63798d70632a81b2
Lucas Gomes <lucasgsm88@gmail.com>
2022-11-20 14:04:56 -0300

    chore: fix wrong param's name

--- src/core/debug.ts
+++ src/core/debug.ts
@@ -13,7 +13,7 @@ try {
 
 /**
  * Prints data in `console.log` if env is `CORDE_DEBUG_MODE`
- * @param data data to be printed
+ * @param infos data to be printed
  */
 export function debug(...infos: any[]) {
   if (process.env.CORDE_DEBUG_MODE) {
